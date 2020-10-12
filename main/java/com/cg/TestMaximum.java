3eec49b4cd9ceb0ce30fee431af1a131a493cfbe
adityak-bit <adityajsr1998@gmail.com>
2020-10-12 07:20:55 +0530

    [ADITYA] Added : Find maximum among 3 integers

--- main/java/com/cg/TestMaximum.java
+++ main/java/com/cg/TestMaximum.java
@@ -2,7 +2,12 @@ package com.cg;
 
 public class TestMaximum {
 
-	public static void main(String[] args) {
-		System.out.println("Test Maximum of 3 variables using Generics");
+	public static Integer testMax(Integer a, Integer b, Integer c) {
+		Integer max = a;
+		if (b.compareTo(max) > 0)
+			max = b;
+		if (c.compareTo(max) > 0)
+			max = c;
+		return max;
 	}
 }
