a68aa31c24f7dce36794d763c24cd60ec8fbbd93
adityak-bit <adityajsr1998@gmail.com>
2020-10-12 07:57:48 +0530

    [ADITYA] Refactored : Created generic class to take in 3 variables of generic type

--- main/java/com/cg/TestMaximum.java
+++ main/java/com/cg/TestMaximum.java
@@ -1,7 +1,15 @@
 package com.cg;
 
-public class TestMaximum {
+public class TestMaximum<E> {
 
+    E a, b, c;
+	
+	public TestMaximum(E a, E b, E c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	
 	public static <E extends Comparable<E>> E testMax(E a, E b, E c) {
 		E max = a;
 		if (b.compareTo(max) > 0)
