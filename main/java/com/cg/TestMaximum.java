efa991e01403158d7c97ef33178c05636833b968
adityak-bit <adityajsr1998@gmail.com>
2020-10-12 07:53:18 +0530

    [ADITYA] Refactored : Find the max using generic method

--- main/java/com/cg/TestMaximum.java
+++ main/java/com/cg/TestMaximum.java
@@ -2,26 +2,8 @@ package com.cg;
 
 public class TestMaximum {
 
-	public static Integer testMax(Integer a, Integer b, Integer c) {
-		Integer max = a;
-		if (b.compareTo(max) > 0)
-			max = b;
-		if (c.compareTo(max) > 0)
-			max = c;
-		return max;
-	}
-
-	public static Float testMax(Float a, Float b, Float c) {
-		Float max = a;
-		if (b.compareTo(max) > 0)
-			max = b;
-		if (c.compareTo(max) > 0)
-			max = c;
-		return max;
-	}
-
-	public static String testMax(String a, String b, String c) {
-		String max = a;
+	public static <E extends Comparable<E>> E testMax(E a, E b, E c) {
+		E max = a;
 		if (b.compareTo(max) > 0)
 			max = b;
 		if (c.compareTo(max) > 0)
